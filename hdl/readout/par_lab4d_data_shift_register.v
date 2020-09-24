8220edcbbe71b77d505cebc394ea545e7a68499d
Patrick Allison <dbarawn@gmail.com>
2020-09-24 11:45:28 -0400

    and new parameterized readouts

---
+++ hdl/readout/par_lab4d_data_shift_register.v
@@ -0,0 +1,202 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////
+// This file is a part of the Antarctic Impulsive Transient Antenna (ANITA)
+// project, a collaborative scientific effort between multiple institutions. For
+// more information, contact Peter Gorham (gorham@phys.hawaii.edu).
+//
+// All rights reserved.
+//
+// Author: Patrick Allison, Ohio State University (allison.122@osu.edu)
+// Author:
+// Author:
+////////////////////////////////////////////////////////////////////////////////
+module par_lab4d_data_shift_register #( parameter NUM_LAB4 = 12,
+					parameter NUM_SRCLK = 12,
+					parameter [NUM_SRCLK-1:0] SRCLK_POLARITY = {NUM_SRCLK{1'b0}},
+					parameter NUM_SS_INCR = 12,
+					parameter LAB4_BITS = 12
+				       
+)(
+		input sys_clk_i,
+		input wclk_i,
+		input readout_i,
+		input readout_rst_i,
+		output done_o,
+		output [LAB4_BITS*NUM_LAB4-1:0] dat_o,
+		output dat_wr_o,
+		input [3:0] prescale_i,
+		output srclk_o,
+		output ss_incr_o,
+		output [6:0] sample_counter_o,
+		output [3:0] bit_counter_o,
+		input [NUM_LAB4-1:0] DOE,
+		output [NUM_SRCLK-1:0] SRCLK,
+		output [NUM_SS_INCR-1:0] SS_INCR,
+		output [15:0] sample_debug
+    );
+
+	// Readout shift register in the LAB4D:
+	// Basically it's just clock out 12 bits and have SS_INCR low in the last clock cycle.
+	// However, there's a quirk: REGCLR, and presumedly power-on, both have the SS_INCR
+	// negative-edge detection screwed up since it puts them both in the 0 state.
+	// So we need to clock in SS_INCR at reset.
+
+	//% Number of bits clocked out each sample.
+	localparam [3:0] MAX_BITS = LAB4_BITS;
+	//% Number of clocks output at reset.
+	localparam [3:0] RESET_CLOCKS = 2;
+	
+	//% Current number of bits in sample.
+	reg [3:0] bit_counter = {4{1'b0}};
+	//% Current number of samples.
+	reg [6:0] sample_counter = {7{1'b0}};
+	//% Next counter value (used for terminal count detection)
+	wire [7:0] sample_counter_plus_one = sample_counter + 1;
+
+	//% The prescale counter slows down everything internal to this block.
+	reg [3:0] prescale_counter = {4{1'b0}};
+
+	//% Goes high when readout_i goes high, and then goes low when state exits IDLE.
+	reg readout_request_seen = 0;
+	
+	//% Flag to capture the shift register data.
+	reg dat_wr = 0;
+	
+	//% Indicates that the first bit is being loaded, which means we don't increment the bit counter.
+	reg loading_first_bit = 0;
+	
+	//% Debug equal to the SRCLK output
+	reg dbg_srclk = 0;
+	//% Debug equal to the SS_INCR output.
+	reg dbg_ss_incr = 1;
+	
+	localparam FSM_BITS=3;
+	localparam [FSM_BITS-1:0] RESET_LOW = 0;
+	localparam [FSM_BITS-1:0] RESET_HIGH = 1;
+	localparam [FSM_BITS-1:0] RESET_LOW_EXIT = 2;
+	localparam [FSM_BITS-1:0] IDLE = 3;
+	localparam [FSM_BITS-1:0] SHIFT_HIGH = 4;
+	localparam [FSM_BITS-1:0] SHIFT_LOW = 5;
+	localparam [FSM_BITS-1:0] SHIFT_LOW_EXIT = 6;
+	localparam [FSM_BITS-1:0] DONE = 7;
+	//% FSM state.
+	reg [FSM_BITS-1:0] state = RESET_LOW;
+		
+	//% Overall clock enable for this entire block.
+	wire ce = (prescale_counter == prescale_i);
+
+	//% Clock enable for SRCLK.
+	wire srclk_ce = ce;
+	//% Data input for SRCLK. Goes high entering RESET_HIGH or SHIFT_HIGH.
+	wire srclk_d = (state == SHIFT_LOW) || (state == IDLE && readout_request_seen) || (state == RESET_LOW);
+	
+	//% Clock enable for SS_INCR.
+	wire ss_incr_ce = (ce) && ((state == IDLE && readout_request_seen) || (state == SHIFT_LOW) || (state == SHIFT_LOW_EXIT));
+	//% Data input for SS_INCR. Goes low at first entry to SHIFT_HIGH, and also at next-to-last bit entry into SHIFT_HIGH, unless last sample.
+	wire ss_incr_d = !(state == IDLE && readout_request_seen) && !(state == SHIFT_LOW && bit_counter == MAX_BITS-2 && !sample_counter_plus_one[7]);
+	
+	//% Clock enable for the shift register.
+	wire shreg_ce = ce && (state == SHIFT_LOW);
+
+	always @(posedge sys_clk_i) begin	
+		// Prescale counter runs all the time.
+		if (readout_rst_i) prescale_counter <= {4{1'b0}};
+		else if (prescale_counter == prescale_i) prescale_counter <= {4{1'b0}};
+		else prescale_counter <= {4{1'b0}};		
+	
+		// Bit counter increments in SHIFT_LOW or RESET_HIGH.
+		if (readout_rst_i) bit_counter <= {4{1'b0}};
+		else if (state == IDLE || loading_first_bit) bit_counter <= {4{1'b0}};
+		else if (ce) begin
+			if (state == SHIFT_LOW) begin
+				if (bit_counter == MAX_BITS-1) bit_counter <= {4{1'b0}};
+				else bit_counter <= bit_counter + 1;
+			end else if (state == RESET_HIGH) begin
+				bit_counter <= bit_counter + 1;
+			end
+		end
+		
+		// Sample counter increments in SHIFT_LOW when bit_counter is at maximum.
+		if (readout_rst_i) sample_counter <= {7{1'b0}};
+		else if (state == IDLE || state == DONE) sample_counter <= {7{1'b0}};
+		else if (state == SHIFT_LOW && bit_counter == MAX_BITS-1) sample_counter <= sample_counter_plus_one;
+
+		// Readout request seen goes high when seeing readout_i, and cleared at SHIFT_HIGH.
+		if (readout_rst_i) readout_request_seen <= 0;
+		else if (readout_i) readout_request_seen <= 1;
+		else if (state == SHIFT_HIGH) readout_request_seen <= 0;
+		
+		if (readout_rst_i) state <= RESET_LOW;
+		else if (ce) begin
+			case (state)
+				// Move to RESET_HIGH
+				RESET_LOW: state <= RESET_HIGH;
+				// If we've counted the max number of clocks, move to exit. Otherwise keep going.
+				RESET_HIGH: if (bit_counter == RESET_CLOCKS-1) state <= RESET_LOW_EXIT;
+								else state <= RESET_LOW;
+				// At exit, go to IDLE.
+				RESET_LOW_EXIT: state <= IDLE;
+				// At idle, if we're waiting for a readout_i, move to SHIFT_HIGH.
+				// Our problem here is the first bit. 
+				IDLE: if (readout_request_seen) state <= SHIFT_HIGH;
+				// At SHIFT_HIGH, if we've clocked in 12 bits (bit_counter increments in SHIFT_LOW)
+				// and we've clocked in 127 samples (127+1 sets sample_counter_plus_one[7]) move to SHIFT_LOW_EXIT.
+				// Otherwise keep going.
+				SHIFT_HIGH: if (bit_counter == MAX_BITS-1 && sample_counter_plus_one[7]) state <= SHIFT_LOW_EXIT;
+								else state <= SHIFT_LOW;
+				// Move to SHIFT_HIGH.
+				SHIFT_LOW: state <= SHIFT_HIGH;
+				// We're done, so exit.
+				SHIFT_LOW_EXIT: state <= DONE;
+				// Back to IDLE.
+				DONE: state <= IDLE;
+			endcase
+		end
+		
+		if (ce) begin
+			if (state == IDLE && readout_request_seen) loading_first_bit <= 1;
+			else if (state == SHIFT_LOW) loading_first_bit <= 0;
+		end
+		
+		// dat_wr can go upon entry to SHIFT_LOW when we've clocked in 12 bits.
+		dat_wr <= ce && (state == SHIFT_HIGH) && (bit_counter == MAX_BITS-1);
+		
+		if (srclk_ce) dbg_srclk <= srclk_d;
+		if (ss_incr_ce) dbg_ss_incr <= ss_incr_d;
+	end
+
+	generate
+		genvar i;
+		for (i=0;i<NUM_LAB4;i=i+1) begin : LAB
+			wire shreg_msb;
+			reg [LAB4_BITS-2:0] data_shreg = {LAB4_BITS-1{1'b0}};
+			// Always have DOE clock in constantly. It just only gets added to the shreg occasionally.
+			(* IOB = "TRUE" *)
+			FDRE #(.INIT(1'b0)) u_shreg(.D(DOE[i]),.CE(1'b1),.C(sys_clk_i),.R(1'b0),.Q(shreg_msb));
+			always @(posedge sys_clk_i) begin : SHREG
+				if (shreg_ce) data_shreg <= {shreg_msb,data_shreg[10:1]};
+			end
+			assign dat_o[12*i +: 12] = {shreg_msb, data_shreg};		   
+		end
+	        for (i=0;i<NUM_SS_INCR;i=i+1) begin : SS
+			(* IOB = "TRUE" *)
+			FDSE #(.INIT(1'b1)) u_ss_incr(.D(ss_incr_d),.CE(ss_incr_ce),.C(sys_clk_i),.S(readout_rst_i),.Q(SS_INCR[i]));
+		end
+	        for (i=0;i<NUM_SRCLK;i=i+1) begin : CL
+			if (SRCLK_POLARITY[i] == 0) begin : POS_POL
+				(* IOB = "TRUE" *)
+				FDRE #(.INIT(1'b0)) u_srclk(.D(srclk_d),.CE(srclk_ce),.C(sys_clk_i),.R(1'b0),.Q(SRCLK[i]));
+			end else begin : NEG_POL
+				(* IOB = "TRUE" *)
+				FDRE #(.INIT(1'b1)) u_srclk(.D(~srclk_d),.CE(srclk_ce),.C(sys_clk_i),.R(1'b0),.Q(SRCLK[i]));
+			end	   
+		end
+	endgenerate
+	assign sample_counter_o = sample_counter;
+	assign bit_counter_o = bit_counter;
+	assign srclk_o = dbg_srclk;
+	assign ss_incr_o = dbg_ss_incr;
+	assign dat_wr_o = dat_wr;
+	assign done_o = (state == DONE);
+	assign sample_debug = {16{1'b0}};
+endmodule
---
+++ hdl/readout/par_lab4d_ram.sv
@@ -0,0 +1,227 @@
+`include "wishbone.vh"
+`timescale 1ns / 1ps
+// This stores all the data from a LAB4 into a FIFO.
+// The address space is 16 bits, and so the LAB4 selection
+// picks off the needed number of top bits to split off.
+// For instance, for 24, the top 5 bits are picked off
+// (lab0 = 0000-07FF)
+// (lab1 = 0800-0FFF)
+// etc.
+module par_lab4d_ram #(
+        parameter NUM_SS_INCR = 2,
+        parameter NUM_SRCLK = 2,
+        parameter [NUM_SRCLK-1:0] SRCLK_POLARITY = {NUM_SRCLK{1'b0}},
+        parameter NUM_LAB4 = 24,
+        parameter [NUM_LAB4-1:0] DOE_POLARITY = {NUM_LAB4{1'b0}},
+        parameter SURF5_COMPAT="FALSE"
+)(
+		input clk_i,
+		input rst_i,
+		`WBS_NAMED_PORT(wb, 32, 16, 4),
+		input dma_lock_i,
+		output dma_locked_o,
+		`WBS_NAMED_PORT(wbdma, 32, 16, 4),
+		input sys_clk_i,
+		input wclk_i,
+		// Readout test pattern control
+		input readout_test_pattern_i,
+		// Readout controls, from the LAB4 controller.
+		input readout_i,
+		input [3:0] readout_header_i,
+		input readout_rst_i,
+		input readout_fifo_rst_i,
+		input [9:0] readout_empty_size_i,
+		output [NUM_LAB4-1:0] readout_fifo_empty_o,
+		input [15:0] prescale_i,
+		
+		output complete_o,
+		output [31:0] readout_debug_o,
+		input [NUM_LAB4-1:0] DOE_LVDS_P,
+		input [NUM_LAB4-1:0] DOE_LVDS_N,
+		output [NUM_SS_INCR-1:0] SS_INCR,
+		output [NUM_SRCLK-1:0] SRCLK_P,
+		output [NUM_SRCLK-1:0] SRCLK_N,
+		
+		output [15:0] sample_debug
+    );
+    localparam L4W=$clog2(NUM_LAB4);
+    localparam L4_BITS = 12;
+    
+	wire [NUM_SRCLK-1:0] SRCLK;
+	wire [NUM_LAB4-1:0] DOE;
+	wire [NUM_LAB4-1:0] DOE_B;
+	generate
+		genvar kk;
+		for (kk=0;kk<NUM_LAB4;kk=kk+1) begin : DIFFLOOP
+			// Polarity swap means we just swap the Ns and Ps. The actual polarity of the SRCLK[kk] signal is swapped in the shift register.
+			if (SRCLK_POLARITY[kk] == 0) begin : POS
+				OBUFDS u_srclk_p(.I(SRCLK[kk]),.O(SRCLK_P[kk]),.OB(SRCLK_N[kk]));
+			end else begin : NEG
+				OBUFDS u_srclk_n(.I(SRCLK[kk]),.O(SRCLK_N[kk]),.OB(SRCLK_P[kk]));
+			end
+			if (DOE_POLARITY[kk] == 0) begin : DOEPOS
+				IBUFDS_DIFF_OUT u_doe_p(.I(DOE_LVDS_P[kk]),.IB(DOE_LVDS_N[kk]),.O(DOE[kk]),.OB(DOE_B[kk]));
+			end else begin : DOENEG
+				IBUFDS_DIFF_OUT u_doe_n(.I(DOE_LVDS_N[kk]),.IB(DOE_LVDS_P[kk]),.O(DOE_B[kk]),.OB(DOE[kk]));
+			end
+		end
+	endgenerate
+		
+	reg use_dma_input = 0;
+	wire [31:0] local_dat_o;
+	wire local_cyc_i;
+	wire local_stb_i;
+	wire local_we_i;
+	wire local_ack_o;
+	wire [15:0] local_adr_i;
+	reg illegal_access_ack = 0;
+	
+	// Guard against changing DMA access during a control cycle.
+	// The reverse (changing DMA access during a DMA cycle) is guarded elsewhere
+	// because the DMA module handles lock/unlock.
+	always @(posedge clk_i) begin
+		if (dma_lock_i && !wb_cyc_i) use_dma_input <= 1;
+		else if (!dma_lock_i && !wb_cyc_i) use_dma_input <= 0;
+	end
+
+	assign local_cyc_i = (use_dma_input) ? wbdma_cyc_i : wb_cyc_i;
+	assign local_stb_i = (use_dma_input) ? wbdma_stb_i : wb_stb_i;
+	assign local_we_i = (use_dma_input) ? wbdma_we_i : wb_we_i;
+	assign local_adr_i = (use_dma_input) ? wbdma_adr_i : wb_adr_i;
+	// The only thing we need to override is the ack - the local bus
+	// will still get data, it'll just be nonsense.
+	assign wb_dat_o = local_dat_o;
+	assign wb_ack_o = (use_dma_input) ? illegal_access_ack : local_ack_o;
+
+    // If SURF5_COMPAT is true, we use 14:11 no matter what.
+    // Otherwise start as far back as needed, and no more.
+    wire [L4W-1:0] l4_sel = (SURF5_COMPAT=="TRUE") ? local_adr_i[14:11] : local_adr_i[16-L4W +: L4W];
+
+
+	// just straight assign these, they're guarded by firmware
+	assign wbdma_dat_o = local_dat_o;
+	assign wbdma_ack_o = local_ack_o;
+	
+	wire data_wr;
+	wire [NUM_LAB4*L4_BITS-1:0] data;
+	// This needs to be expanded to a proper power of 2...
+	localparam DATA_OUT_SIZE = (1<<L4W);
+	wire [31:0] data_out[DATA_OUT_SIZE-1:0];
+	wire [11:0] lab_read;
+
+	// We're now using a FWFT FIFO, with no embedded registers, but we register the data ourselves.
+	reg [31:0] data_out_registered = {32{1'b0}};
+
+	reg [9:0] readout_empty_threshold = {10{1'b0}};
+
+	// So the state machine now goes IDLE -> ACK immediately, and then back to IDLE.
+	// (making it a bit of a pointless state machine).
+	// fifo_read gets asserted in IDLE, which means the data changes in ACK.
+	// If everything stays asserted, that means data_out_registered is actually valid immediately again.
+	// If we make burst reads work, need to figure out how that works
+	//          state   new_fifo_read  cti data_out data_out_registered
+	// clock 0: IDLE    1               0   D[0]     X
+	// clock 1: ACK     1               0   D[1]     D[0]
+	// clock 2: ACK     0               7   D[2]     D[1]
+	// so new_fifo_read would be (local_cyc_i && local_stb_i && !local_we_i) && ((state==IDLE) || (state==ACK && cti_i != 7))
+	// *should* work..... can probably simplify this to just && !(cti_i == 7 && local_ack_o)
+	
+	localparam FSM_BITS=2;
+	localparam [FSM_BITS-1:0] IDLE = 0;
+	localparam [FSM_BITS-1:0] READ = 1;
+	localparam [FSM_BITS-1:0] WTF = 2;
+	localparam [FSM_BITS-1:0] ACK = 3;
+	reg [FSM_BITS-1:0] state = IDLE;
+	always @(posedge clk_i) begin
+		// buy time for the empty threshold
+		readout_empty_threshold <= readout_empty_size_i;
+	
+		if (wb_cyc_i && wb_stb_i && use_dma_input && !illegal_access_ack) illegal_access_ack <= 1;
+		else illegal_access_ack <= 0;
+		
+		case (state)
+			IDLE: if (local_cyc_i && local_stb_i && !local_we_i) state <= ACK;	// read is 1 here
+			READ: state <= ACK;														// data is at output here
+			WTF: state <= ACK;														// why is this here?!?!?
+			ACK: state <= IDLE;														// data is at register here
+		endcase
+	end
+
+	wire fifo_read = (local_cyc_i && local_stb_i && !local_we_i) && (state == IDLE);
+	assign local_ack_o = (state == ACK);
+	
+	always @(posedge clk_i) begin
+		data_out_registered <= data_out[l4_sel];
+	end
+	assign local_dat_o = data_out_registered;
+
+	reg [L4_BITS-1:0] readout_test_pattern = {L4_BITS{1'b0}};
+	reg data_wr_reg = 0;
+	always @(posedge sys_clk_i) begin
+		if (readout_i && readout_test_pattern_i) readout_test_pattern <= {L4_BITS{1'b0}};
+		else if (readout_test_pattern_i && data_wr) readout_test_pattern <= readout_test_pattern + 1;
+
+		data_wr_reg <= data_wr;
+	end
+	
+	generate
+		genvar i;
+		for (i=0;i<DATA_OUT_SIZE;i=i+1) begin : FIFOS
+		    if (i < NUM_LAB4) begin : RL
+                wire [15:0] data_input;
+                wire [31:0] data_output;
+                reg [15:0] data_in_reg = {16{1'b0}};
+                // Each LAB gets 512 entries of space, or 2048 bytes: so e.g. from 0000-07FF.
+                // So the lab selection picks off bits [14:11].
+                // It's a FIFO, not actually a RAM, because we don't have enough address space.
+                assign lab_read[i] = fifo_read && (local_adr_i[14:11] == i);
+                always @(posedge sys_clk_i) begin : DATA_INPUT_REGISTER
+                    if (data_wr) begin
+                        if (readout_test_pattern_i) data_in_reg <= {readout_header_i, readout_test_pattern};
+                        else data_in_reg <= {readout_header_i, data[L4_BITS*i +: L4_BITS]};
+                    end
+                end
+                // Data write and data are delayed to let them come from registers.
+                lab4d_fifo u_fifo(.wr_clk(sys_clk_i),.wr_en(data_wr_reg),.din(data_in_reg),
+                                        .rst(readout_fifo_rst_i),.prog_empty(readout_fifo_empty_o[i]),
+                                        .prog_empty_thresh(readout_empty_threshold),
+                                        .rd_clk(clk_i),.rd_en(lab_read[i]),.dout(data_output));
+                assign data_out[i] = {data_output[15:0],data_output[31:16]};
+            end else begin : SHAD
+                // We want to shadow things to keep the decode easy, so what we do is just shift
+                // DATA_OUT_SIZE down by 1 and use that as a modulo. We don't just do modulo NUM_LAB4,
+                // because that wraps to *increase* the coding: i.e. 24 = 0, so 0 matches 0 *and* 24,
+                // but not 16, so everyone still needs all the bits.
+                // Whereas this way 24 maps to 8, meaning for 8-15 it just
+                // ignores the top bit, *reducing* the coding.
+                assign data_out[i] = data_out[ i % (DATA_OUT_SIZE>>1) ];
+            end
+        end
+	endgenerate
+	
+	assign dma_locked_o = use_dma_input;
+	
+	wire dbg_ss_incr;
+	wire dbg_srclk;
+	wire [6:0] sample_counter;
+	wire [3:0] bit_counter;
+	par_lab4d_data_shift_register #(.NUM_LAB4(NUM_LAB4),.NUM_SS_INCR(NUM_SS_INCR),.NUM_SRCLK(NUM_SRCLK))
+	                                          u_shreg(.sys_clk_i(sys_clk_i),.wclk_i(wclk_i),
+													  .readout_i(readout_i),.readout_rst_i(readout_rst_i),
+													  .done_o(complete_o),.dat_o(data),
+													  .dat_wr_o(data_wr),.prescale_i(prescale_i),
+													  .ss_incr_o(dbg_ss_incr),.srclk_o(dbg_srclk),
+													  .bit_counter_o(bit_counter),
+													  .sample_counter_o(sample_counter),.DOE(DOE),.SS_INCR(SS_INCR),.SRCLK(SRCLK),
+													  .sample_debug(sample_debug));
+    // this is stupid fix this for Vivado
+	assign readout_debug_o[0 +: L4_BITS] = data[0 +: L4_BITS];
+	assign readout_debug_o[12] = DOE[0];
+	assign readout_debug_o[13] = data_wr;
+	assign readout_debug_o[14] = readout_i;
+	assign readout_debug_o[15] = dbg_ss_incr;
+	assign readout_debug_o[16] = dbg_srclk;
+	assign readout_debug_o[17] = complete_o;
+	assign readout_debug_o[18 +: 7] = sample_counter;
+	assign readout_debug_o[25 +: 4] = bit_counter;
+endmodule
